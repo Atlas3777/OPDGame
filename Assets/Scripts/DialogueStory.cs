1e37a5288ce95beb646c3dacf3ff4e08c21c6f0c
semakol <kolyvanov.sema@yandex.ru>
2024-05-11 13:37:55 +0500

    v0.7.1

--- Assets/Scripts/DialogueStory.cs
+++ Assets/Scripts/DialogueStory.cs
@@ -6,6 +6,7 @@ using Cinemachine;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class DialogueStory : MonoBehaviour
@@ -17,7 +18,9 @@ public class DialogueStory : MonoBehaviour
 
     public TextMeshProUGUI text_panel;
 
-    public Transform panel;
+    public Transform ButtonsPanel;
+
+    public TextMeshProUGUI Character_panel;
 
     private Dictionary<string, Story> _storiesDictionary;
 
@@ -26,9 +29,11 @@ public class DialogueStory : MonoBehaviour
     [Serializable]
     public struct Story
     {
+        
         [field: SerializeField] public string Tag {get; private set;}
         [field: SerializeField] public string Text {get; private set;}
         [field: SerializeField] public string StopTag {get; private set;}
+        [field: SerializeField] public string Character {get; private set;}
         [field: SerializeField] public Answer[] Answers {get; private set;}
     }
 
@@ -39,6 +44,7 @@ public class DialogueStory : MonoBehaviour
         [field: SerializeField] public string Text {get; private set;}
         [field: SerializeField] public string ResponseText {get; private set;}
         [field: SerializeField] public string NewText {get; private set;}
+        [field: SerializeField] public UnityEvent Action {get; private set;}
         public bool IsUsed = false;
     }
 
@@ -64,22 +70,25 @@ public class DialogueStory : MonoBehaviour
     private void ChangeStory(string tag, string Newtext = "")
     {
         var children = new List<GameObject>();
-        foreach (Transform child in panel) children.Add(child.gameObject);
+        foreach (Transform child in ButtonsPanel) children.Add(child.gameObject);
         children.ForEach(child => Destroy(child));
         var story = _storiesDictionary[tag];
+        Character_panel.text = story.Character;
         if (Newtext != "" || Newtext != string.Empty) text_panel.text = Newtext;
         else text_panel.text = story.Text;
         foreach (var answer in story.Answers)
         {
             if (answer.IsUsed) continue;
-            GameObject button = Instantiate(MainButton, panel);
+            GameObject button = Instantiate(MainButton, ButtonsPanel);
             var text = button.GetComponentInChildren<TextMeshProUGUI>();
             text.text = answer.Text;
+            button.GetComponent<Button>().onClick.AddListener(() => answer.Action.Invoke());
             if (answer.ResponseText == "stop") 
                 button.GetComponent<Button>().onClick.AddListener(() => StopStory());
             else
                 button.GetComponent<Button>().onClick.AddListener(() => 
                 {
+                    
                     //FileReader.Termins["st"] = true;
                     FileReader.Write(story.Text);
                     FileReader.Write(text.text);
@@ -94,7 +103,7 @@ public class DialogueStory : MonoBehaviour
                 }
                 );
         }
-        LayoutRebuilder.ForceRebuildLayoutImmediate(panel.GetComponent<RectTransform>());
+        LayoutRebuilder.ForceRebuildLayoutImmediate(ButtonsPanel.GetComponent<RectTransform>());
         if (story.Answers.All(x => x.IsUsed)) ChangeStory(story.StopTag, Newtext);
     }
 
--- Assets/Scripts/DictionaryScript.cs
+++ Assets/Scripts/DictionaryScript.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DictionaryScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void TriggerOpen() {
+        GetComponent<BoxCollider>().isTrigger = true;
     }
 }
